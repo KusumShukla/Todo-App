3b5af4be5071f43ac824941e6958f791973e31bb
kusum shukla <kshukla.shukla@gmail.com>
2023-06-12 18:32:04 +0530

    updating second dev1 branch

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,7 @@ const Layout = () => {
       <AuthStatus />
       <header className="app-title">
         <h2>Todo App</h2>
+        <h3>Here we updated new dev1 branch</h3>
       </header>
 
       <Outlet />
